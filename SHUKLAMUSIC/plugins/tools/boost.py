abb5062b8344d4479c1867463baf3a2305305622
XFLSWAN <158449266+XFLSWAN@users.noreply.github.com>
2025-11-15 12:47:33 +0530

    Update boost.py

--- SHUKLAMUSIC/plugins/tools/boost.py
+++ SHUKLAMUSIC/plugins/tools/boost.py
@@ -1,15 +1,3 @@
-# -----------------------------------------------
-# 🔸 StrangerMusic Project
-# 🔹 Developed & Maintained by: Shashank Shukla (https://github.com/itzshukla)
-# 📅 Copyright © 2025 – All Rights Reserved
-#
-# 📖 License:
-# This source code is open for educational and non-commercial use ONLY.
-# You are required to retain this credit in all copies or substantial portions of this file.
-# Commercial use, redistribution, or removal of this notice is strictly prohibited
-# without prior written permission from the author.
-#
-# ❤️ Mad
 import re
 from os import getenv
 from dotenv import load_dotenv
